8509b7b2be5585d1517c4b63abced2224dd3af1a
romo <robert.moore@zuhlke.com>
2019-03-25 10:55:59 +0000

    Label L-systems in Main

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ import lsystem.kochcurve.KochCurve;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\nDragon Curve\n");
         System.out.println(new DragonCurve().draw(10));
 
-        System.out.println("\n\n\n\n");
-
+        System.out.println("\nFractal Binary Tree\n");
         System.out.println(new FractalBinaryTree().draw(5));
 
-        System.out.println("\n\n\n\n");
-
+        System.out.println("\nKoch Curve\n");
         System.out.println(new KochCurve().draw(3));
     }
 }
