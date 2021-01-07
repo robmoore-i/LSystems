0617117e766e0f25c86dfcd54b05eeb5d062cbb3
romo <robmoore121@gmail.com>
2021-01-07 12:24:21 +0800

    Small error handling for script

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,1 +1,7 @@
+if [[ $(pwd | xargs basename) != "LSystems" ]]; then
+  echo "Not building docker image.
+This script should be executed from the repository's root directory."
+  exit 1
+fi
+
 docker build -f docker/Dockerfile . -t lsystems
